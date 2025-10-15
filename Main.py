b826929bf88f250a2895cf25b0455344a5bb5707
Nima <nimaj1382@gmail.com>
2025-10-16 01:57:07 +0330

    Refactor main execution

--- Main.py
+++ Main.py
@@ -1,12 +1,8 @@
 from Classes import *
 
 if __name__ == "__main__":
-    new_project = Project("My Project", "This is a new project.")
+    me = User(username="Nima")
+    me.add_project(Project(project_name="TestProject"))
 
-    second_project = Project("My Project 2", "This is another project.")
-
-    new_project.add_task(Task(task_name="first task"))
-    print(new_project.project_tasks)
-
-    print(new_project)
-    print(second_project)
+    me.show_projects()
+    print(me)
