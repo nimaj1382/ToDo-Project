188199cf785871c2129ad2c356f2d3c3d96ff843
Nima <nimaj1382@gmail.com>
2025-10-16 05:21:32 +0330

    Refactor Main.py to a new example of usage

--- Main.py
+++ Main.py
@@ -1,8 +1,19 @@
-from Classes import *
+from User import User
+
+from Project import Project
+
+from Task import Task
 
 if __name__ == "__main__":
     me = User(username="Nima")
-    me.add_project(Project(project_name="TestProject"))
+    project = Project(project_name="TestProject1")
+    project2 = Project(project_name="TestProject2")
+    me.add_project(project)
+    me.add_project(project2)
+    project.add_task(Task(task_name="Task1", task_description="This is task 1"))
+    project.add_task(Task(task_name="Task2", task_description="This is task 2"))
+    project2.add_task(Task(task_name="Task3", task_description="This is task 3"))
 
-    me.show_projects()
     print(me)
+    me.show_projects()
+    me.show_tasks()
