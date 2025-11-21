c7afba1011d8c40881973c12761f3e41f2292c63
Nima <nimaj1382@gmail.com>
2025-11-21 20:04:54 +0330

    feat: add task.py model file and set up Task class

---
+++ app/models/task.py
@@ -0,0 +1,28 @@
+from sqlalchemy import (Column, Integer, String, ForeignKey, Enum,
+                        DateTime, UniqueConstraint)
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+import enum
+
+class TaskStatus(enum.Enum):
+    TODO = "todo"
+    DOING = "doing"
+    DONE = "done"
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(30), nullable=False, index=True)
+    description = Column(String(150))
+    status = Column(Enum(TaskStatus), default=TaskStatus.TODO, nullable=False)
+    due_date = Column(DateTime)
+
+    project_id = Column(Integer, ForeignKey("projects.id", ondelete="CASCADE"),
+                        nullable=False)
+    project = relationship("Project", back_populates="tasks")
+
+    __table_args__ = (
+        UniqueConstraint('project_id', 'title', name='uq_project_task_name')
+    )
