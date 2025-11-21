9d45d754492bb431e23100729c47f9d94ec2b06d
Nima <nimaj1382@gmail.com>
2025-11-21 16:16:26 +0330

    feat: add __repr__ method to User class

--- app/models/user.py
+++ app/models/user.py
@@ -6,3 +6,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
+
+    def __repr__(self):
+        return f"User: id={self.id} username={self.username}"
