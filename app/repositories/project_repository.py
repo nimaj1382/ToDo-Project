33a4476fc26f927df2866d67a350d56b12785adb
Nima <nimaj1382@gmail.com>
2025-11-28 21:46:49 +0330

    feat: add get_project_by_name method

--- app/repositories/project_repository.py
+++ app/repositories/project_repository.py
@@ -12,6 +12,9 @@ class ProjectRepository:
     def get_project_by_id(self, project_id: int) -> Optional[Project]:
         return self.session.query(Project).filter(Project.id == project_id).first()
 
+    def get_project_by_name(self, project_name: str) -> Optional[Project]:
+        return self.session.query(Project).filter(Project.name == project_name).first()
+
     def add_project(self, project: Project) -> None:
         self.session.add(project)
         self.session.commit()
