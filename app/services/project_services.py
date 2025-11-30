f9f2e2ec4d44a689dfd3712c1102d450f0ad609d
Nima <nimaj1382@gmail.com>
2025-11-30 22:22:02 +0330

    feat: add method for printing all of projects with their tasks

--- app/services/project_services.py
+++ app/services/project_services.py
@@ -106,7 +106,7 @@ class ProjectService:
                                         task_due_date=task_due_date,
                                         task_service=task_service)
 
-    def project_tasks_list(self, project: Project) -> List['Task']:
+    def project_tasks_list(self, project: Type[Project]) -> List[Type['Task']]:
         return self.repository.project_tasks_list(project)
 
     def project_tasks_list_by_id(self, project_id: int) -> List['Task']:
@@ -169,7 +169,7 @@ class ProjectService:
         for project in all_projects:
             self.print_project(project, indent)
 
-    def print_project(self, project: Type[Project], indent:int = 0) -> None:
+    def print_project(self, project: Type[Project], indent: int = 0) -> None:
         load_dotenv()
         max_name_length = int(os.getenv("MAX_SHOW_NAME_LENGTH", 10))
         max_description_length = int(os.getenv("MAX_SHOW_DESCRIPTION_LENGTH", 15))
@@ -179,3 +179,25 @@ class ProjectService:
         display_name = str(project.name)[:max_name_length] + textwrap.shorten(str(project.name)[max_name_length + 1:], width=3, placeholder="...")
         display_description = str(project.description)[:max_description_length] + textwrap.shorten(str(project.description)[max_description_length + 1:], width=3, placeholder="...")
         print(f"{tab_indent}{project.id:<15} \t {display_name:<{max_name_length}} \t {display_description:<{max_description_length}}")
+
+    def print_all_projects_with_tasks(self, task_service: 'TaskService', indent: int = 0):
+        load_dotenv()
+        max_name_length = int(os.getenv("MAX_SHOW_NAME_LENGTH", 10))
+        max_description_length = int(os.getenv("MAX_SHOW_DESCRIPTION_LENGTH", 15))
+        max_due_date_length = int(os.getenv("MAX_SHOW_DUE_DATE_LENGTH", 10))
+        tab_indent = '\t' * indent
+
+        all_projects = self.all_projects()
+
+        print(f"{tab_indent}{'project id':^15} \t {'project name':^{max_name_length}} \t {'project description':^{max_description_length}}")
+        print()
+
+        for project in all_projects:
+            self.print_project(project, indent)
+            project_tasks = self.project_tasks_list(project)
+            if len(project_tasks) > 0:
+                print(f"{'tasks':-^{max_name_length + max_description_length + 20}}")
+                for task in project_tasks:
+                    task_service.print_task(task, indent + 1)
+                print(f"{'end of tasks':-^{max_name_length + max_description_length + 20}}")
+                print("\n\n")
