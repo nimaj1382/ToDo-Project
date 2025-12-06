3873a570b8c749941b965fbca6f8b38d2798db81
Nima <nimaj1382@gmail.com>
2025-12-06 17:50:50 +0330

    refactor: set maximum line length to 100

--- app/services/project_services.py
+++ app/services/project_services.py
@@ -28,7 +28,8 @@ class ProjectService:
         # Check uniqueness of project name
 
         if self.get_project_by_name(project_name) != None:
-            raise UniquenessError("Project name must be unique. The given project name is already in use.")
+            raise UniquenessError("Project name must be unique. "
+                                  "The given project name is already in use.")
 
         project = Project(name = project_name, description = project_description)
         self.repository.add_project(project)
@@ -123,7 +124,8 @@ class ProjectService:
 
     def set_project_name(self, project: Project, new_project_name: str) -> None:
         if self.get_project_by_name(new_project_name):
-            raise UniquenessError("Project name must be unique. The given project name is already in use.")
+            raise UniquenessError("Project name must be unique. "
+                                  "The given project name is already in use.")
         self.repository.set_project_name(project, new_project_name)
 
     def set_project_name_by_id(self, project_id: int, new_project_name: str) -> None:
@@ -147,7 +149,8 @@ class ProjectService:
             raise ExistanceError("project with given id does not exist.")
         self.set_project_description(project, new_project_description)
 
-    def set_project_description_by_name(self, project_name: str, new_project_description: str) -> None:
+    def set_project_description_by_name(self, project_name: str,
+                                        new_project_description: str) -> None:
         project = self.get_project_by_name(project_name)
         if project is None:
             raise ExistanceError("project with given name does not exist.")
@@ -163,7 +166,10 @@ class ProjectService:
         max_description_length = int(os.getenv("MAX_SHOW_DESCRIPTION_LENGTH", 15))
         tab_indent = '\t' * indent
 
-        print(f"{tab_indent}{'project id':^15} \t|{'project name':^{max_name_length}} \t|{'project description':^{max_description_length}}")
+        print(f"{tab_indent}"
+              f"{'project id':^15} \t"
+              f"|{'project name':^{max_name_length}} \t"
+              f"|{'project description':^{max_description_length}}")
         print()
 
         for project in all_projects:
@@ -176,9 +182,16 @@ class ProjectService:
         max_due_date_length = int(os.getenv("MAX_SHOW_DUE_DATE_LENGTH", 10))
         tab_indent = '\t' * indent
 
-        display_name = str(project.name)[:max_name_length] + textwrap.shorten(str(project.name)[max_name_length + 1:], width=3, placeholder="...")
-        display_description = str(project.description)[:max_description_length] + textwrap.shorten(str(project.description)[max_description_length + 1:], width=3, placeholder="...")
-        print(f"{tab_indent}{project.id:<15} \t|{display_name:<{max_name_length}} \t|{display_description:<{max_description_length}}")
+        display_name = (str(project.name)[:max_name_length] +
+                        textwrap.shorten(str(project.name)[max_name_length + 1:],
+                                         width=3, placeholder="..."))
+        display_description = (str(project.description)[:max_description_length] +
+                            textwrap.shorten(str(project.description)[max_description_length + 1:],
+                                         width=3, placeholder="..."))
+        print(f"{tab_indent}"
+              f"{project.id:<15} \t"
+              f"|{display_name:<{max_name_length}} \t"
+              f"|{display_description:<{max_description_length}}")
 
     def print_all_projects_with_tasks(self, task_service: 'TaskService', indent: int = 0):
         load_dotenv()
@@ -189,7 +202,10 @@ class ProjectService:
 
         all_projects = self.all_projects()
 
-        print(f"{tab_indent}{'project id':^15} \t|{'project name':^{max_name_length}} \t|{'project description':^{max_description_length}}")
+        print(f"{tab_indent}"
+              f"{'project id':^15} \t"
+              f"|{'project name':^{max_name_length}} \t"
+              f"|{'project description':^{max_description_length}}")
         print()
 
         for project in all_projects:
